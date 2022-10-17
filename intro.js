d3918182a069e1250c388ca9827ddf5d2095d875
Luciano Piehl <luchopinamar123@gmail.com>
2022-10-16 21:11:55 -0300

    Primer commit

---
+++ intro.js
@@ -0,0 +1,28 @@
+let intro = document.querySelector('.Intro');
+let logo = document.querySelector('.LogoIntro');
+let logoSpan = document.querySelectorAll('.IntroPartes');
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+       logoSpan.forEach((span, index) => {
+            setTimeout(() => {
+               span.classList.add('active');
+            }, (index + 1) * 100);
+       });
+
+       setTimeout(() => {
+          logoSpan.forEach((span, index) => {
+            setTimeout(() => {
+               span.classList.remove('active');
+               span.classList.add('fade'); 
+            }, (span + 1) * 80);
+          });
+       }, 3300);
+
+       setTimeout(() => {
+          intro.style.top  = '-100vh';
+       }, 2800); //1
+
+    });
+
+});
